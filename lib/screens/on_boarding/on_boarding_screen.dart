e6e13d1ac86dd916f53200c330103de689e89789
Noor-378 <nooraldenmoh378@gmail.com>
2025-03-02 18:27:58 +0300

    finish the onBoard images

--- lib/screens/on_boarding/on_boarding_screen.dart
+++ lib/screens/on_boarding/on_boarding_screen.dart
@@ -9,17 +9,17 @@ class OnBoardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     List<BoardingModel> boarding = [
       BoardingModel(
-        image: "assets/images/onboard_1.png",
+        image: "assets/images/onboarding_1.png",
         title: "title1",
         body: "body1",
       ),
       BoardingModel(
-        image: "assets/images/onboard_2.png",
+        image: "assets/images/onboarding_2.png",
         title: "title2",
         body: "body2",
       ),
       BoardingModel(
-        image: "assets/images/onboard_3.png",
+        image: "assets/images/onboarding_3.png",
         title: "title3",
         body: "body3",
       ),
