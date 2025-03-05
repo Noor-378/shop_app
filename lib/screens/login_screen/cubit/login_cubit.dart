b137dd766071a8e23ff5cd57b4f86913404f7350
Noor-378 <nooraldenmoh378@gmail.com>
2025-03-05 10:17:19 +0300

    situp the login ligic

--- lib/screens/login_screen/cubit/login_cubit.dart
+++ lib/screens/login_screen/cubit/login_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/screens/login_screen/cubit/login_state.dart';
+import 'package:shop_app/shared/network/end_points.dart';
+import 'package:shop_app/shared/network/remote/dio_helper.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitialState());
   static LoginCubit get(context) => BlocProvider.of(context);
-  void userLogin(){}
+  void userLogin({
+    required String email,
+    required String password,
+  }) {
+    DioHelper.postData(
+      url: LOGIN,
+      data: {
+        "email": email,
+        "password": password,
+      },
+    );
+  }
 }
--- lib/shared/network/remote/dio_helper.dart
+++ lib/shared/network/remote/dio_helper.dart
@@ -4,9 +4,11 @@ class DioHelper {
   static late Dio dio;
   static init() {
     dio = Dio(BaseOptions(
-      baseUrl: "https://student.valuxapps.com/api/",
-      receiveDataWhenStatusError: true,
-    ));
+        baseUrl: "https://student.valuxapps.com/api/",
+        receiveDataWhenStatusError: true,
+        headers: {
+          "Content-Type": "application/json",
+        }));
   }
 
   static Future<Response> getData({
@@ -16,6 +18,18 @@ class DioHelper {
     return await dio.get(
       url,
       queryParameters: query,
+    );
+  }
+
+  static Future<Response> postData({
+    required String url,
+     Map<String, dynamic>? query,
+    required Map<String, dynamic> data,
+  }) async {
+    return dio.post(
+      url,
+      queryParameters: query,
+      data: data,
     );
   }
 }
