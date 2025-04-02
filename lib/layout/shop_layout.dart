212ac6283f931cf5fba29cc11d155b33d7f507d9
Noor-378 <nooraldenmoh378@gmail.com>
2025-04-02 14:16:27 +0300

    create shop layout

---
+++ lib/layout/shop_layout.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ShopLayout extends StatelessWidget {
+  const ShopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Placeholder(),
+    );
+  }
+}
