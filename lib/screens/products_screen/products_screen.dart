74aae4561b44aa38edf79be920b8ffa6ed8b07f5
Noor-378 <nooraldenmoh378@gmail.com>
2025-04-04 17:48:30 +0300

    create products builder

--- lib/screens/products_screen/products_screen.dart
+++ lib/screens/products_screen/products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/screens/products_screen/products_builder.dart';
 import 'package:shop_app/shared/cubit/cubit/cubit.dart';
 import 'package:shop_app/shared/cubit/states/states.dart';
 
@@ -13,12 +14,7 @@ class ProductsScreen extends StatelessWidget {
       builder: (context, state) {
         return AppCubit.get(context).homeModel == null
             ? CircularProgressIndicator()
-            : Center(
-                child: Text(
-                  "Products Screen",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              );
+            : ProductsBuilder();
       },
     );
   }
