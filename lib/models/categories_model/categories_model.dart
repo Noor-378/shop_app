8e0911d60257692415e96ce3058d37de5a49e375
Noor-378 <nooraldenmoh378@gmail.com>
2025-04-07 00:13:36 +0300

    finish the categories model

--- lib/models/categories_model/categories_model.dart
+++ lib/models/categories_model/categories_model.dart
@@ -1,3 +1,37 @@
 class CategoriesModel {
-  
+  bool? status;
+  CategoriesDataModel? data;
+  CategoriesModel.fromJson(Map<String, dynamic> json) {
+    status = json["status"];
+    data = CategoriesDataModel.fromJson(json["data"]);
+  }
+}
+
+class CategoriesDataModel {
+  int? currentPage;
+  List<DataModel> data = [];
+
+  CategoriesDataModel.fromJson(Map<String, dynamic> json) {
+    currentPage = json["current_page"];
+    json["data"].forEach(
+      (element) {
+        data.add(
+          DataModel.fromJson(
+            json["data"],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class DataModel {
+  int? id;
+  String? page;
+  String? image;
+  DataModel.fromJson(Map<String, dynamic> json) {
+    id = json["id"];
+    page = json["page"];
+    image = json["image"];
+  }
 }
